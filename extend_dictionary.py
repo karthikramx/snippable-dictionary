7380b53d94133a9dab3a3af947ad386b3db0b3ef
Karthik Ram <karthikram570@gmail.com>
2021-04-26 09:23:01 +0530

    mod

--- extend_dictionary.py
+++ extend_dictionary.py
@@ -1,2 +1,15 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+
+english_dictionary = pd.read_csv("english_dictionary.csv")
+
+
+english_dictionary["useage"] = np.nan
+english_dictionary["etymology"] = np.nan
+english_dictionary["synonymns"] = np.nan
+english_dictionary["antonyms"] = np.nan
+
+english_dictionary.to_csv("english_dictionary_extended.csv")
+
 
